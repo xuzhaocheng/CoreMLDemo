4682366a5cfece44ab1d23c3cccfcf1117c0a0fc
Xu, Zhaocheng <zhaxu@microstrategy.com>
2018-11-27 10:06:33 +0800

    Add fruits model

--- CreateML.playground/Contents.swift
+++ CreateML.playground/Contents.swift
@@ -1,12 +1,12 @@
 import CreateML
 import Foundation
 
-let trainingDir = URL(fileURLWithPath: "/Users/zhaxu/Downloads/training")
+let trainingDir = URL(fileURLWithPath: "/Users/zhaxu/Downloads/fruit-images-for-object-detection/train")
 
-let testDir = URL(fileURLWithPath: "/Users/zhaxu/Downloads/testing")
+let testDir = URL(fileURLWithPath: "/Users/zhaxu/Downloads/fruit-images-for-object-detection/test")
 
 let model = try MLImageClassifier(trainingData: .labeledDirectories(at: trainingDir))
 
 let evaluation = model.evaluation(on: .labeledDirectories(at: testDir))
 
-try model.write(to: URL(fileURLWithPath: "/Users/zhaxu/Desktop/AnimalsModel"))
+try model.write(to: URL(fileURLWithPath: "/Users/zhaxu/Desktop/FruitsModel"))
